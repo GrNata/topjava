33a44d6cf94f88013847ac67d26e77b810e69974
GrNata <gr.natalij@gmail.com>
2020-07-31 15:20:58 +0300

    hw6-5

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,30 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 public class MealService {
 
     private MealRepository repository;
 
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    public void update(Meal meal, int userId) {
+        Assert.notNull(meal, "meal must not be null");
+        checkNotFoundWithId(repository.save(meal, userId), meal.id());
+    }
+
+    public Meal create(Meal meal, int userId) {
+        Assert.notNull(meal, "meal must not be null");
+        return repository.save(meal, userId);
+    }
+
+    public Meal getWithUser(int id, int userId) {
+        return checkNotFoundWithId(repository.getWithUser(id, userId), id);
+    }
 }

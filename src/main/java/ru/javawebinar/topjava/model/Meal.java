ed66d36190ff95853bf3dec36860acb7d4419ffc
GrNata <gr.natalij@gmail.com>
2020-07-31 11:46:30 +0300

    hw1-1

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Meal {
     private final LocalDateTime dateTime;
@@ -29,4 +30,8 @@ public class Meal {
     }
 
     public LocalDate getDate()  {   return dateTime.toLocalDate();  }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
 }

af397540230c4cace13b79893786331314b91643
GrNata <gr.natalij@gmail.com>
2020-08-15 07:46:24 +0300

    View

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -3,5 +3,11 @@ package ru.javawebinar.topjava;
 import javax.validation.groups.Default;
 
 public class View {
+
+    public interface JsonREST {}
+    public interface JsonUI {}
+
+    public interface ValidatedUI {}
+
     public interface Persist extends Default {}
 }
